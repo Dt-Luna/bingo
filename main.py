5dc312595f50ef05ab7a43eb865c0e3b316cbbb3
Dt-Luna <luna.deodato@escolar.ifrn.edu.br>
2025-01-30 15:56:08 -0300

    função de sortear

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import random
-def gerar_cartela(m):
-    if m == 1:
+
+modo_de_jogo = int(input())
+numero_maximo = 0
+if modo_de_jogo == 1:
+    numero_maximo = 40
+elif modo_de_jogo == 0:
+    numero_maximo = 30
+numeros_sorteados = []
+numeros_possiveis = [n for n in range(1, numero_maximo + 1)]
+
+def gerar_cartela():
+    if modo_de_jogo == 1:
         ncartelas = 4
         ncolunas = 4
         nlinhas = 3
-    elif m == 0:
+    elif modo_de_jogo == 0:
         ncartelas = 2
         ncolunas = 3
         nlinhas = 2
-
     cartelas = []
     for _ in range(ncartelas):
         a, b = 1, 11
@@ -26,7 +35,21 @@ def gerar_cartela(m):
         cartelas.append(cartela)
     return cartelas
 
-print(gerar_cartela(1))
+def sortear_numero():
+    sorteado = random.choice(numeros_possiveis)
+    numeros_sorteados.append(sorteado)
+    numeros_possiveis.remove(sorteado)
+    return(sorteado)
 
-        
+def comecar_jogo():
+    
 
+
+    print(gerar_cartela())
+
+    i=0
+    while i < 30:
+        print(sortear_numero())
+        i+=1
+
+comecar_jogo()
